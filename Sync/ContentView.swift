fa9a0609b49b5190985727d01a2b2552fa69be49
Paul <colandreapaul@gmail.com>
2024-05-23 09:59:02 -0400

    Initial Commit

---
+++ Sync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sync
+//
+//  Created by Paul  Colandrea on 5/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sync/SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncApp.swift
+//  Sync
+//
+//  Created by Paul  Colandrea on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
