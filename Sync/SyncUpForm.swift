599a38a13caf1c451bccb49ed62afde83476b5ba
Paul <colandreapaul@gmail.com>
2024-05-28 14:57:20 -0400

    add base add reducer

--- Sync/SyncUpForm.swift
+++ Sync/SyncUpForm.swift
@@ -5,14 +5,43 @@
 //  Created by Paul  Colandrea on 5/28/24.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
-struct SyncUpForm: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+@Reducer
+struct SyncUpForm {
+    @ObservableState
+    struct State {
+        var syncUp: SyncUp
+    }
+    
+    enum Action: BindableAction {
+      case binding(BindingAction<State>)
+      case onDeleteAttendees(IndexSet)
+        case addAttendeeButtonTapped
+    }
+    
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        
+        Reduce { state, action in
+            switch action {
+            case .binding:
+                return .none
+                
+            case let .onDeleteAttendees(indices):
+                state.syncUp.attendees.remove(atOffsets: indices)
+                if state.syncUp.attendees.isEmpty {
+                    state.syncUp.attendees.append(
+                        Attendee(id: Attendee.ID())
+                    )
+                }
+                return .none
+            }
+        }
     }
 }
 
-#Preview {
-    SyncUpForm()
-}
+//#Preview {
+//    SyncUpForm()
+//}
--- Sync/SyncUpsList.swift
+++ Sync/SyncUpsList.swift
@@ -6,13 +6,121 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
-struct SyncUpsList: View {
+struct SyncUpsListView: View {
+    let store: StoreOf<SyncUpsList>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(store.syncUps) { syncUp in
+                Button {
+                    
+                } label: {
+                    CardView(syncUp: syncUp)
+                }
+                .listRowBackground(syncUp.theme.mainColor)
+            }
+            .onDelete{ indexSet in
+                store.send(.onDelete(indexSet))
+            }
+        }.toolbar {
+            Button {
+                store.send(.addSyncUpButtonTapped)
+            } label: {
+                Image(systemName: "plus")
+            }
+        }
+        .navigationTitle("Daily Sync-Ups")
     }
 }
 
+@Reducer
+struct SyncUpsList {
+    @ObservableState
+    struct State {
+        var syncUps: IdentifiedArrayOf<SyncUp> = []
+    }
+    
+    enum Action {
+        case addSyncUpButtonTapped
+        case onDelete(IndexSet)
+        case onSyncUpTapped(id: SyncUp.ID)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .addSyncUpButtonTapped:
+                return .none
+                
+            case let .onDelete(indexSet):
+                state.syncUps.remove(atOffsets: indexSet)
+                return .none
+                
+            case .onSyncUpTapped:
+                return .none
+            }
+            
+        }
+    }
+}
+
+struct CardView: View {
+    let syncUp: SyncUp
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(syncUp.title).font(.headline)
+            Spacer()
+            HStack() {
+                Label("\(syncUp.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label(syncUp.duration.formatted(.units()), systemImage: "clock")
+                                    .labelStyle(.trailingIcon)
+            }.font(.caption)
+        }
+        .padding()
+        .foregroundColor(syncUp.theme.accentColor)
+    }
+}
+
+struct TrailingIconLabelStyle: LabelStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    HStack {
+      configuration.title
+      configuration.icon
+    }
+  }
+}
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+  static var trailingIcon: Self { Self() }
+}
+
+
 #Preview {
-    SyncUpsList()
+  NavigationStack {
+    SyncUpsListView(
+      store: Store(
+        initialState: SyncUpsList.State(
+          syncUps: [
+            SyncUp(
+              id: SyncUp.ID(),
+              attendees: [
+                Attendee(id: Attendee.ID(), name: "Blob"),
+                Attendee(id: Attendee.ID(), name: "Blob Jr."),
+                Attendee(id: Attendee.ID(), name: "Blob Sr."),
+              ],
+              title: "Point-Free Morning Sync"
+            )
+          ]
+        )
+      ) {
+        SyncUpsList()
+              ._printChanges()
+      }
+    )
+  }
 }
